3a185f5a5440cc022ef2ebdac13cb7c902128c48
Felix Villalobos <felix.evl@gmail.com>
2019-04-29 16:01:27 -0500

    Updates

---
+++ precache-manifest.24fd8bff2ba131a62378ee2518615e3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cde3feec99da0ebd323fc3bb7bbd085",
+    "url": "/Game-of-Clicks/index.html"
+  },
+  {
+    "revision": "0520f390dd0f2801049c",
+    "url": "/Game-of-Clicks/static/css/main.bfaaba51.chunk.css"
+  },
+  {
+    "revision": "8b28b04b5b06edd2dc22",
+    "url": "/Game-of-Clicks/static/js/2.dc948b19.chunk.js"
+  },
+  {
+    "revision": "0520f390dd0f2801049c",
+    "url": "/Game-of-Clicks/static/js/main.5983be51.chunk.js"
+  },
+  {
+    "revision": "3313b5ba536593f64d4b",
+    "url": "/Game-of-Clicks/static/js/runtime~main.6b66b836.js"
+  },
+  {
+    "revision": "3af178ecd64adfa098e7456e5830e5f8",
+    "url": "/Game-of-Clicks/static/media/Game of Thrones.3af178ec.ttf"
+  }
+]);
